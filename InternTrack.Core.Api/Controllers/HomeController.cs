db6a64aeec4ac759257c02db8063d522f59975d4
Hassan Habib <hassanhabib@live.com>
2023-04-20 16:24:44 -0700

    INFRA: Initialize Project

---
+++ InternTrack.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternTrack.Core.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Thank you, Mario. But the princess is in another castle!");
+    }
+}
---
+++ InternTrack.Core.Api/Program.cs
@@ -0,0 +1,22 @@
+// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace InternTrack.Core.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>());
+        }
+    }
+}
