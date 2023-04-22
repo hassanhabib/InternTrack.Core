1d1480d6adbf37dfc40e6a4a04da17e376a7aab5
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-04-21 22:09:13 -0700

    FOUNDATION: Implemented Provision Sql Server

--- InternTrack.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/CloudManagementService.cs
+++ InternTrack.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/CloudManagementService.cs
@@ -6,7 +6,9 @@
 using InternTrack.Core.Api.Infrastructure.Provision.Brokers.Clouds;
 using InternTrack.Core.Api.Infrastructure.Provision.Brokers.Logging;
 using InternTrack.Core.Api.Infrastructure.Provision.Services.Foundations.CloudMangaments;
+using Microsoft.Azure.Management.AppService.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.Sql.Fluent;
 
 namespace InternTrack.Core.Api.Infrastructure.Provision.Services.Foundations.CloudManagements
 {
@@ -25,7 +27,7 @@ namespace InternTrack.Core.Api.Infrastructure.Provision.Services.Foundations.Clo
         {
             string resourceGroupName = $"{projectName}-RESOURCE-{environment}".ToUpper();
 
-            LoggingBroker.LogActivity(message: $"Provisioning {resourceGroupName}");
+            LoggingBroker.LogActivity(message: $"Provisioning {resourceGroupName}...");
 
             IResourceGroup resourceGroup =
                 await CloudBroker.CreateResourceGroupAsync(
@@ -34,6 +36,38 @@ namespace InternTrack.Core.Api.Infrastructure.Provision.Services.Foundations.Clo
             LoggingBroker.LogActivity(message: $"{resourceGroupName} Provisioned");
 
             return resourceGroup;
+        }
+
+        public async ValueTask<IAppServicePlan> ProvisionPlanAsync(string projectName, string environment, IResourceGroup resourceGroup)
+        {
+            string planName = $"{projectName}-PLAN-{environment}".ToUpper();
+
+            LoggingBroker.LogActivity(message: $"Provisiong {planName}...");
+
+            IAppServicePlan plan =
+                await CloudBroker.CreatePlanAsync(
+                    planName,
+                    resourceGroup);
+
+            LoggingBroker.LogActivity(message: $"{plan} Provisioned");
+
+            return plan;
+        }
+
+        public async ValueTask<ISqlServer> ProvisionSqlServerAsync(string projectName, string environment, IResourceGroup resourceGroup)
+        {
+            string sqlServerName = $"{projectName}-dbserver-{environment}".ToLower();
+            
+            LoggingBroker.LogActivity(message: $"Provisioning {projectName}...");
+
+            ISqlServer sqlServer = 
+                await CloudBroker.CreateSqlServerAsync( 
+                    sqlServerName, 
+                    resourceGroup);
+
+            LoggingBroker.LogActivity(message: $"{sqlServerName} Provisioned)");
+
+            return sqlServer;
         }
     }
 }
--- InternTrack.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/ICloudManagementService.cs
+++ InternTrack.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/ICloudManagementService.cs
@@ -3,7 +3,9 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
+using Microsoft.Azure.Management.AppService.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.Sql.Fluent;
 
 namespace InternTrack.Core.Api.Infrastructure.Provision.Services.Foundations.CloudMangaments
 {
@@ -12,6 +14,24 @@ namespace InternTrack.Core.Api.Infrastructure.Provision.Services.Foundations.Clo
         ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
             string projectName,
             string environment
+            );
+
+        ValueTask<IAppServicePlan> ProvisionPlanAsync(
+                string projectName,
+                string environment,
+                IResourceGroup resourceGroup
+            );
+
+        ValueTask<ISqlServer> ProvisionSqlServerAsync(
+                string projectName,
+                string environment,
+                IResourceGroup resourceGroup
+            );
+
+        ValueTask<ISqlDatabase> ProvisionSqlDatabaseAsync(
+                string projectName,
+                string environment,
+                ISqlServer sqlServer
             );
     }
 }
