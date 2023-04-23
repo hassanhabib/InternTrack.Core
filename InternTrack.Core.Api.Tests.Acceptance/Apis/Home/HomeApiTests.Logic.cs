3183ea60d34e894b3cf51394ca2a07e6ee070590
Manasa1191 <bmanasa07@gmail.com>
2023-04-22 23:05:59 -0700

    INFRA Initialize Acceptance Tests - Pass

--- InternTrack.Core.Api.Tests.Acceptance/Apis/Home/HomeApiTests.Logic.cs
+++ InternTrack.Core.Api.Tests.Acceptance/Apis/Home/HomeApiTests.Logic.cs
@@ -10,7 +10,7 @@ namespace InternTrack.Core.Api.Tests.Acceptance.Apis.Home
         [Fact]
         public async Task ShouldReturnHomeMessageAsync()
         {
-            string expectedMessage = "Thank you, Mario. But the princess is in another castler!";
+            string expectedMessage = "Thank you, Mario. But the princess is in another castle!";
             string actualMessage = await this.internTrackApiBroker.GetHomeMessageAsync();
             actualMessage.Should().BeEquivalentTo(expectedMessage);
         }
