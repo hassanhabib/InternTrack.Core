f3da784d2a9287cb96456e2204875f00b8dd3593
adi latic <adideenlatic@gmail.com>
2023-04-28 15:12:49 -0500

    INFRA: Initialize Unit Tests Project

---
+++ Intern.Core.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+namespace Intern.Core.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+
+}
