c01fe65ac30115305249fcafcf78bcc1d1cc40b3
adi latic <adideenlatic@gmail.com>
2023-04-30 14:58:35 -0500

    CONFIGS: Add References

--- Intern.Core.Api.Tests.Unit/DeleteMe.cs
+++ Intern.Core.Api.Tests.Unit/DeleteMe.cs
@@ -2,12 +2,13 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using Xunit;
+
 namespace Intern.Core.Api.Tests.Unit
 {
     public class DeleteMe
     {
         [Fact]
         public void ShouldBeTrue() => Assert.True(true);
-    }
-
+    } 
 }
