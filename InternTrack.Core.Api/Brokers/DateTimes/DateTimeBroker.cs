bab3c18dc4ed787be065877168ca8672a09d83f0
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-05-01 08:29:59 -0700

    CODERUB: Added copyright

--- InternTrack.Core.Api/Brokers/DateTimes/DateTimeBroker.cs
+++ InternTrack.Core.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//-------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
 
 namespace InternTrack.Core.Api.Brokers.DateTimes
 {
