400f0962257746d1ae82e48d74af6db9beb37198
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-04-30 22:40:24 -0700

    INFRA: Initialize Build Project

---
+++ InternTrack.Core.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,81 @@
+﻿
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using System.Collections.Generic;
+
+namespace InternTrack.Core.Infrastructure.Build
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "Sofee Core Build Pipeline",
+
+                OnEvents = new Events
+                {
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "main" }
+                    },
+
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "main" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.Windows2022,
+
+                        Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Checking Out Code"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Installing .NET",
+
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "7.0.203",
+                        IncludePrerelease = true
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restoring Nuget Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Building Project"
+                },
+
+                new TestTask
+                {
+                    Name = "Running Tests"
+                }
+            },
+                    }
+                }
+            };
+
+            var client = new ADotNetClient();
+
+            client.SerializeAndWriteToFile(
+                adoPipeline: githubPipeline,
+                path: "../../../../.github/workflows/dotnet.yml");
+        }
+    }
+}
