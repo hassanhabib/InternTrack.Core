003d91cc6a185af8a05391f1f98003c10b329c87
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-05-05 23:25:14 -0700

    CODERUB: Removed comment

--- InternTrack.Core.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.cs
+++ InternTrack.Core.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.cs
@@ -6,15 +6,6 @@
 using Azure.Identity;
 using Azure.ResourceManager;
 
-/* 
-Environment Variables naming convention:
-AZURE_CLIENT_ID
-AZURE_CLIENT_SECRET
-AZURE_TENANT_ID
-AZURE_ADMIN_NAME
-AZURE_ADMIN_Password
- */
-
 namespace InternTrack.Core.Api.Infrastructure.Provision.Brokers.Clouds
 {
     public partial class CloudBroker : ICloudBroker
