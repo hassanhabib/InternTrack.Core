3feb5ccaa770522270d251c0ce5c3755eadafd39
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-05-05 20:17:16 -0700

    BROKER: Moved Logging to Api layer

---
+++ InternTrack.Core.Api/Brokers/Logging/ILoggingBroker.cs
@@ -0,0 +1,19 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System;
+
+namespace InternTrack.Core.Api.Infrastructure.Provision.Brokers.Logging
+{
+    public interface ILoggingBroker
+    {
+        void LogInformation(string message);
+        void LogTrace(string message);
+        void LogDebug(string message);
+        void LogWarning(string message);
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
