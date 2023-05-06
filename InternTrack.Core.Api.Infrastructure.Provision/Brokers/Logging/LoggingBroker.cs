45f728170322d2e13bb4f5ecbb6d8523baa2c894
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-05-05 20:33:33 -0700

    BROKER: Reverted Provision LoggingBroker to original state

--- InternTrack.Core.Api.Infrastructure.Provision/Brokers/Logging/LoggingBroker.cs
+++ InternTrack.Core.Api.Infrastructure.Provision/Brokers/Logging/LoggingBroker.cs
@@ -10,27 +10,7 @@ namespace InternTrack.Core.Api.Infrastructure.Provision.Brokers.Logging
 {
     public class LoggingBroker : ILoggingBroker
     {
-        private readonly ILogger logger;
-
-        public LoggingBroker(ILogger logger) =>
-            this.logger = logger;
-
-        public void LogInformation(string message) =>
-            this.logger.LogInformation(message);
-
-        public void LogTrace(String message) =>
-            this.logger.LogTrace(message);
-
-        public void LogDebug(string message) =>
-            this.logger.LogDebug(message);
-
-        public void LogWarning(string message) =>
-            this.logger.LogWarning(message);
-
-        public void LogError(Exception exception) =>
-            this.logger.LogError(exception.Message, exception);
-
-        public void LogCritical(Exception exception) =>
-            this.logger.LogCritical(exception, exception.Message);
+        public void LogActivity(string message) =>
+            Console.WriteLine(message);
     }
 }
