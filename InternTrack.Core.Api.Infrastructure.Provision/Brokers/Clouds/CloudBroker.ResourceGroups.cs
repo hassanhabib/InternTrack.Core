a06d72fdd46ef4a1273c02e01cb0ad67928425a0
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-05-06 17:05:36 -0700

    BROKER: Refactored CreateResourceGroupAsync

--- InternTrack.Core.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.ResourceGroups.cs
+++ InternTrack.Core.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.ResourceGroups.cs
@@ -15,18 +15,14 @@ namespace InternTrack.Core.Api.Infrastructure.Provision.Brokers.Clouds
         public async ValueTask<ResourceGroupResource> CreateResourceGroupAsync(
             string resourceGroupName)
         {
-            SubscriptionResource subscription = await client
-                .GetDefaultSubscriptionAsync();
-
-            ResourceGroupCollection resourceGroups = subscription
-                .GetResourceGroups();
-
             var resourceGroupData = new ResourceGroupData(
                 AzureLocation.WestUS3);
 
-            ArmOperation<ResourceGroupResource> operation = await resourceGroups
-                .CreateOrUpdateAsync(
-                    WaitUntil.Completed,
+            ArmOperation<ResourceGroupResource> operation = await client
+                .GetDefaultSubscriptionAsync()
+                .Result
+                .GetResourceGroups()
+                .CreateOrUpdateAsync(WaitUntil.Completed,
                     resourceGroupName,
                     resourceGroupData);
 
