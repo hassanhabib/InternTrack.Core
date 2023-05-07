a23ce1834ac3f336f1dabba3c621c7e5afc94e7e
Manasa1191 <bmanasa07@gmail.com>
2023-05-07 13:49:57 -0700

    DATA: Intern Model & Migrations

---
+++ InternTrack.Core.Api/InternDbContext.cs
@@ -0,0 +1,15 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using InternTrack.Core.Api.Models.Interns;
+using System.Data.Entity;
+
+namespace InternTrack.Core.Api
+{
+    public class InternDbContext : DbContext
+    {
+        public DbSet<Intern> Interns { get; set; }
+    }
+}
---
+++ InternTrack.Core.Api/Migrations/202305071736152_AddIntern.cs
@@ -0,0 +1,45 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Data.Entity.Migrations;
+
+namespace InternTrack.Core.Api.Migrations
+{
+    public partial class AddIntern : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Interns",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(nullable: false),
+                    JoinDate = table.Column<DateTimeOffset>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: false),
+                    UpdatedBy = table.Column<string>(nullable: false),
+                    TimeZone = table.Column<string>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Interns", t => t.Id);
+                });
+            
+        }
+        
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Interns");
+        }
+    }
+}
