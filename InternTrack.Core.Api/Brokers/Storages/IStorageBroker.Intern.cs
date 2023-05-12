e7d1e91e491efc2b585c3837ebd0e7386d68ea0c
adi latic <adideenlatic@gmail.com>
2023-05-12 15:05:03 -0500

    BROKERS: Select ID

--- InternTrack.Core.Api/Brokers/Storages/IStorageBroker.Intern.cs
+++ InternTrack.Core.Api/Brokers/Storages/IStorageBroker.Intern.cs
@@ -8,11 +8,9 @@ namespace InternTrack.Core.Api.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Intern> InsertInternAsync(Intern intern);
-
         IQueryable<Intern> SelectAllIntern();
-
-        ValueTask<Intern> SelectPostByIdAsync(Guid internId);
-
+        ValueTask<Intern> SelectInternByIdAsync(Guid internId);
         ValueTask<Intern> DeleteInternAsync(Intern intern);
+        ValueTask<Intern> UpdateInternAsync(Intern intern);
     }
 }
