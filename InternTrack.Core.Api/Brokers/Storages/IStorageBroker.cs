10ebe4dde5e8f3396422c0d99f5e21a122fb51cd
adi latic <adideenlatic@gmail.com>
2023-05-25 18:51:37 -0500

    BROKERS: Ran code clean up, Added copyright

--- InternTrack.Core.Api/Brokers/Storages/IStorageBroker.cs
+++ InternTrack.Core.Api/Brokers/Storages/IStorageBroker.cs
@@ -2,15 +2,10 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
-using InternTrack.Core.Api.Models.Interns;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Storage;
-
 namespace InternTrack.Core.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
-       
+
     }
 }
