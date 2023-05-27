f71c55cdf5b7671922b2bb210881f2d34290946e
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-05-26 22:02:59 -0700

    BROKER: Intialized ApplicationInsight Create

--- InternTrack.Core.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.ApplicationInsight.cs
+++ InternTrack.Core.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.ApplicationInsight.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Azure.Core;
+using Azure.ResourceManager.ApplicationInsights.Models;
+using Azure.ResourceManager.ApplicationInsights;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager;
+using Azure;
 
 namespace InternTrack.Core.Api.Infrastructure.Provision.Brokers.Clouds
 {
-    public class CloudBroker
+    public partial class CloudBroker
     {
+        public async ValueTask<ApplicationInsightsComponentResource> CreateApplicationInsightComponentAsync(
+            string componentName,
+            ResourceGroupResource resourceGroup)
+        {
+            ApplicationInsightsComponentCollection appInsightCollection = resourceGroup.GetApplicationInsightsComponents();
+
+            var appInsightData = new ApplicationInsightsComponentData(AzureLocation.WestUS, "web")
+            {
+                ApplicationType = ApplicationType.Web,
+                FlowType = FlowType.Bluefield,
+                RequestSource = RequestSource.Rest
+            };
+
+            ArmOperation<ApplicationInsightsComponentResource> appInsightComponent =
+                await appInsightCollection.CreateOrUpdateAsync(
+                    WaitUntil.Completed,
+                    componentName,
+                    appInsightData);
+
+            return appInsightComponent.Value;
+        }
     }
 }
