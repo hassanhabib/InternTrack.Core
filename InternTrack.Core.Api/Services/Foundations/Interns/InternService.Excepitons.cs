69295c57723d433aa0334d04a1cd0aef5a57736e
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-05-31 22:39:58 -0700

    FOUNDATIONS: Added Handling excetions for InternService

--- InternTrack.Core.Api/Services/Foundations/Interns/InternService.Excepitons.cs
+++ InternTrack.Core.Api/Services/Foundations/Interns/InternService.Excepitons.cs
@@ -1,6 +1,121 @@
-﻿namespace InternTrack.Core.Api.Services.Foundations.Interns
+﻿using System;
+using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
+using InternTrack.Core.Api.Models.Interns;
+using InternTrack.Core.Api.Models.Interns.Exceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Xeptions;
+
+namespace InternTrack.Core.Api.Services.Foundations.Interns
 {
     public partial class InternService
     {
+        private delegate ValueTask<Intern> ReturningInternFunction();
+
+        private async ValueTask<Intern> TryCatch(ReturningInternFunction returningInternFunction)
+        {
+            try
+            {
+                return await returningInternFunction();
+            }
+            catch (NullInternException nullInternException)
+            {
+                throw CreateAndLogValidationException(nullInternException);
+            }
+            catch (SqlException sqlException)
+            {
+                var failedInternStorageException =
+                    new FailedInternStorageException(sqlException);
+
+                throw CreateAndLogCriticalDependencyException(failedInternStorageException);
+            }
+            catch (InvalidInternException invalidInternException)
+            {
+                throw CreateAndLogValidationException(invalidInternException);
+            }
+            catch (NotFoundInternException notFoundInternException)
+            {
+                throw CreateAndLogValidationException(notFoundInternException);
+            }
+            catch (DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistsInternException =
+                    new AlreadyExistsInternException(duplicateKeyException);
+
+                throw CreateAndLogDependencyValidationException(alreadyExistsInternException);
+            }
+            catch (ForeignKeyConstraintConflictException foreignKeyConstraintConflictException)
+            {
+                var invalidInternReferenceException =
+                    new InvalidInternReferenceException(foreignKeyConstraintConflictException);
+
+                throw CreateAndLogDependencyValidationException(invalidInternReferenceException);
+            }
+            catch (DbUpdateException databaseUpdateException)
+            {
+                var failedStorageInternException =
+                    new FailedInternStorageException(databaseUpdateException);
+
+                throw CreateAndLogDependencyException(failedStorageInternException);
+            }
+            catch (Exception exception)
+            {
+                var failedInternServiceException =
+                    new FailedInternServiceException(exception);
+
+                throw CreateAndLogServiceException(failedInternServiceException);
+            }
+        }
+
+        private InternServiceException CreateAndLogServiceException(Xeption exception)
+        {
+            var InternServiceException =
+                new InternServiceException(exception);
+
+            this.loggingBroker.LogError(InternServiceException);
+
+            return InternServiceException;
+        }
+
+        private InternDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var InternDependencyException =
+                new InternDependencyException(exception);
+
+            this.loggingBroker.LogCritical(InternDependencyException);
+
+            return InternDependencyException;
+        }
+
+        private InternValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var InternValidationException =
+                new InternValidationException(exception);
+
+            this.loggingBroker.LogError(InternValidationException);
+
+            return InternValidationException;
+        }
+
+        private InternDependencyValidationException CreateAndLogDependencyValidationException(Xeption exception)
+        {
+            var InternDependencyValidationException =
+                new InternDependencyValidationException(exception);
+
+            this.loggingBroker.LogError(InternDependencyValidationException);
+
+            return InternDependencyValidationException;
+        }
+
+        private InternDependencyException CreateAndLogDependencyException(Xeption exception)
+        {
+            var InternDependencyException =
+                new InternDependencyException(exception);
+
+            this.loggingBroker.LogError(InternDependencyException);
+
+            return InternDependencyException;
+        }
     }
 }
