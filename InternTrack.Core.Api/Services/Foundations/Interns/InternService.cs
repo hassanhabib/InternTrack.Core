8c166565a7f1c7928fb235bf94dfb04a5937d70f
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-05-31 22:25:35 -0700

    FOUNDATION: Injected dependencies for InternService

--- InternTrack.Core.Api/Services/Foundations/Interns/InternService.cs
+++ InternTrack.Core.Api/Services/Foundations/Interns/InternService.cs
@@ -1,6 +1,23 @@
-﻿namespace InternTrack.Core.Api.Services.Foundations.Interns
+﻿using InternTrack.Core.Api.Brokers.DateTimes;
+using InternTrack.Core.Api.Brokers.Loggings;
+using InternTrack.Core.Api.Brokers.Storages;
+
+namespace InternTrack.Core.Api.Services.Foundations.Interns
 {
     public partial class InternService
     {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public InternService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
     }
 }
