08b57ee61683bacca5769d65bbdc1fb561dca30d
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-05-31 22:27:43 -0700

    FOUNDATION: Intialized CreateInternAsync in interface

--- InternTrack.Core.Api/Services/Foundations/Interns/IInternService.cs
+++ InternTrack.Core.Api/Services/Foundations/Interns/IInternService.cs
@@ -1,6 +1,10 @@
-﻿namespace InternTrack.Core.Api.Services.Foundations.Interns
+﻿using System.Threading.Tasks;
+using InternTrack.Core.Api.Models.Interns;
+
+namespace InternTrack.Core.Api.Services.Foundations.Interns
 {
     public interface IInternService
     {
+        ValueTask<Intern> CreateInternAsync(Intern intern);
     }
 }
