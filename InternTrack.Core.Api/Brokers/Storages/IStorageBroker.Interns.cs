7d237d65d7f1140884d0358f390a8919ae10d2c5
adi latic <adideenlatic@gmail.com>
2023-05-31 21:19:32 -0500

    CODE RUB: Interface Fix

--- InternTrack.Core.Api/Brokers/Storages/IStorageBroker.Interns.cs
+++ InternTrack.Core.Api/Brokers/Storages/IStorageBroker.Interns.cs
@@ -12,6 +12,8 @@ namespace InternTrack.Core.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Intern> DeleteInternAsync(Intern intern);
+        ValueTask<Intern> InsertInternAsync(Intern intern);
         ValueTask<Intern> SelectInternByIdAsync(Guid internId);
     }
 }
