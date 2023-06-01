04ccc3134cb1b717a477e44b3cce669f31312f6d
adi latic <adideenlatic@gmail.com>
2023-05-31 21:05:14 -0500

    CODE RUB: Insert FIx

--- InternTrack.Core.Api/Brokers/Storages/StorageBroker.Interns.cs
+++ InternTrack.Core.Api/Brokers/Storages/StorageBroker.Interns.cs
@@ -3,14 +3,37 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
+using System.Threading.Tasks;
 using InternTrack.Core.Api.Models.Interns;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace InternTrack.Core.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Intern> Interns { get; set; }
-    
+
+        public async ValueTask<Intern> DeleteInternAsync(Intern intern)
+        {
+            using var broker =
+                 new StorageBroker(this.configuration);
+
+            EntityEntry<Intern> internEntityEntry =
+                broker.Interns.Remove(intern);
+
+            await broker.SaveChangesAsync();
+
+            return internEntityEntry.Entity;
+        }
+            
+        public async ValueTask<Intern> InsertInternAsync(Intern intern)
+        {
+            var broker = new StorageBroker(this.configuration);
+            EntityEntry<Intern> internEntityEntry = await broker.AddAsync(intern);
+            await broker.SaveChangesAsync();
+
+            return internEntityEntry.Entity;
+        }
     }
 }
