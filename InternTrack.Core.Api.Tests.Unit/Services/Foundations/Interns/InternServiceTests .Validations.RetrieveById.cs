b935ca19bc3a48b9ff1802a6a6668ce2134a2da7
adi latic <adideenlatic@gmail.com>
2023-06-13 18:02:22 -0500

    ShouldThrowValdationExceptionOnRetrieveByIdIfIdIsInValidAndLogItAsync => FAIL

---
+++ InternTrack.Core.Api.Tests.Unit/Services/Foundations/Interns/InternServiceTests .Validations.RetrieveById.cs
@@ -0,0 +1,104 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using InternTrack.Core.Api.Models.Interns;
+using InternTrack.Core.Api.Models.Interns.Exceptions;
+using Moq;
+using Xunit;
+
+namespace InternTrack.Core.Api.Tests.Unit.Services.Foundations.Interns
+{
+    public partial class InternServiceTests
+    {
+
+        [Fact]
+        public async Task ShouldThrowValdationExceptionOnRetrieveByIdIfIdIsInValidAndLogItAsync()
+        {
+            // given
+            Guid randomInternId = default;
+            Guid inputInternId = randomInternId;
+
+            var invalidInternException = new InvalidInternException();
+
+            invalidInternException.AddData(
+                key: nameof(Intern.Id),
+                values: "id is required");
+
+            var expectedInternValidationException =
+            new InternValidationException(invalidInternException);
+
+            // when
+            ValueTask<Intern> retrieveInternByIdTask =
+                this.internService.RetrieveInternByIdAsync(inputInternId);
+
+            // then
+            await Assert.ThrowsAsync<InternValidationException>(() =>
+                retrieveInternByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionsAs(
+                        expectedInternValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectInternByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnRetrieveWhenStorageStudentIsNullAndLogItAsync()
+        {
+            // given
+            Guid randomInternId = Guid.NewGuid();
+            Guid inputInternId = randomInternId;
+            Intern invalidStorageIntern = null;
+            var notFoundInternException = new NotFoundInternException(inputInternId);
+
+            var expectedInternValidationException =
+                new InternValidationException(notFoundInternException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectInternByIdAsync(inputInternId))
+                    .ReturnsAsync(invalidStorageIntern);
+
+            // when
+            ValueTask<Intern> retrieveInternByIdTask =
+                this.internService.RetrieveInternByIdAsync(inputInternId);
+
+            // then
+            await Assert.ThrowsAsync<InternValidationException>(() =>
+                retrieveInternByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionsAs(
+                    expectedInternValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectInternByIdAsync(inputInternId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+
+        }
+    }
+}
---
+++ InternTrack.Core.Api/Models/Interns/Exceptions/NotFoundInternException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace InternTrack.Core.Api.Models.Interns.Exceptions
+{
+    public class NotFoundInternException : Xeption
+    {
+        public NotFoundInternException(Guid internId)
+                : base(message: $"Couldn't find intern with id: {internId}.") { }
+    }
+}
