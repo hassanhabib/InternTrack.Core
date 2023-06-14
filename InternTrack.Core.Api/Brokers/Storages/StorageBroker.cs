6c057dcbcb8fcdd0f08d7819d08bfa4e0ebf7dd8
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-06-13 23:26:46 -0700

    CODERUB: Deleted uncessary code

--- InternTrack.Core.Api/Brokers/Storages/StorageBroker.cs
+++ InternTrack.Core.Api/Brokers/Storages/StorageBroker.cs
@@ -2,10 +2,7 @@
 // Copyright (c) Coalition of the Good-Hearted Engineers
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
-<<<<<<< HEAD
 
-=======
->>>>>>> 2f01c36c3700d66a50c3c4ca5e60201cd403a40c
 using EFxceptions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
