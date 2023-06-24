a47bf09103820af243441ac56227215cb505f65a
adi latic <adideenlatic@gmail.com>
2023-06-23 20:50:47 -0500

    ShouldThrowValidatonExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync ->FAIL

---
+++ InternTrack.Core.Api.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using InternTrack.Core.Api.Models.Interns.Exceptions;
+using InternTrack.Core.Api.Models.Interns;
+using Moq;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+
+namespace InternTrack.Core.Api.Tests.Unit.Services.Foundations.Interns
+{
+    public partial class InternServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidInternId = Guid.Empty;
+            var invalidInternException = new InvalidInternException();
+
+            invalidInternException.AddData(
+               key: nameof(Intern.Id),
+               values: "Id is required");
+
+            var expectedInternValidationException =
+                new InternValidationException(invalidInternException);
+
+            // when
+            ValueTask<Intern> actualInternTask =
+                this.internService.RetrieveInternByIdAsync(invalidInternId);
+
+            // then
+            await Assert.ThrowsAsync<InternValidationException>(() => actualInternTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameValidationExceptionAs(
+                    expectedInternValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectInternByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
