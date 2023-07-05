f701ee09cb45454c56ce6bafe4e629952887a7a8
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-07-05 10:52:03 -0700

    CODERUB: Deleted uneccessary code

--- InternTrack.Core.Api/Controllers/InternController.cs
+++ InternTrack.Core.Api/Controllers/InternController.cs
@@ -3,7 +3,6 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
-<<<<<<< HEAD
 using System.Linq;
 using System.Threading.Tasks;
 using InternTrack.Core.Api.Models.Interns;
