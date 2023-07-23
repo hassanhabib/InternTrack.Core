f1eab357b28d664ca3af7d4d17a7c3a69ee3c50c
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-07-23 12:19:47 -0700

    CODERUB: added space

--- InternTrack.Core.Api/Models/Interns/Exceptions/LockedInternException.cs
+++ InternTrack.Core.Api/Models/Interns/Exceptions/LockedInternException.cs
@@ -1,6 +1,16 @@
-﻿namespace InternTrack.Core.Api.Models.Interns.Exceptions
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace InternTrack.Core.Api.Models.Interns.Exceptions
 {
-    public class LockedInternException
+    public class LockedInternException : Xeption
     {
+        public LockedInternException(Exception innerException)
+            : base(message: "Locked intern record exception, please try again later. ", innerException){ }
     }
 }
