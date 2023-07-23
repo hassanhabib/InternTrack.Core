f64bf09607f47d83f9fa859f2d9262a147d37a7e
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-07-23 12:24:22 -0700

    CODERUB: Reverted changes

--- InternTrack.Core.Api/Models/Interns/Exceptions/LockedInternException.cs
+++ InternTrack.Core.Api/Models/Interns/Exceptions/LockedInternException.cs
@@ -11,6 +11,6 @@ namespace InternTrack.Core.Api.Models.Interns.Exceptions
     public class LockedInternException : Xeption
     {
         public LockedInternException(Exception innerException)
-            : base(message: "Locked intern record exception, please try again later. ", innerException){ }
+            : base(message: "Locked intern record exception, please try again later.", innerException){ }
     }
 }
