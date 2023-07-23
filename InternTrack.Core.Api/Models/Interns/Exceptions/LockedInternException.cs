5acfe5f7c498b2afab473d513d722ead542d25e0
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-07-23 13:01:57 -0700

    CODErUB: Resloved Merge Conflict

--- InternTrack.Core.Api/Models/Interns/Exceptions/LockedInternException.cs
+++ InternTrack.Core.Api/Models/Interns/Exceptions/LockedInternException.cs
@@ -11,13 +11,8 @@ namespace InternTrack.Core.Api.Models.Interns.Exceptions
     public class LockedInternException : Xeption
     {
         public LockedInternException(Exception innerException)
-<<<<<<< HEAD
-            : base(message: "Locked intern record exception, please try again later.", innerException) { }
-    }
-}
-=======
             : base(message: "Locked intern record exception, please try again later.", innerException)
         { }
     }
 }
->>>>>>> 5b57a6d88016de5bd5fbae28f6c8b431f0b260aa
+
