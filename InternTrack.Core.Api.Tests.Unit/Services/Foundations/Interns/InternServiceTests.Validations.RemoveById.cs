44e8c7cdf93e0ca972284980c4b29e31ca06429f
adi latic <alatic1234@gmail.com>
2023-07-24 23:23:10 -0500

    ShouldThrowNotFoundExceptionOnRemoveIfInternIsNotFoundAndLogitAsync -> FAIL

--- InternTrack.Core.Api.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Validations.RemoveById.cs
+++ InternTrack.Core.Api.Tests.Unit/Services/Foundations/Interns/InternServiceTests.Validations.RemoveById.cs
@@ -57,5 +57,53 @@ namespace InternTrack.Core.Api.Tests.Unit.Services.Foundations.Interns
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemoveIfInternIsNotFoundAndLogitAsync()
+        {
+            // given
+            Guid randomInternId = Guid.NewGuid();
+            Guid inputInternId = randomInternId;
+            Intern noIntern = null;
+
+            var notFoundInternException =
+                new NotFoundInternException(inputInternId);
+
+            var expectedInternValidationException =
+                new InternValidationException(notFoundInternException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectInternByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noIntern);
+
+            // when
+            ValueTask<Intern> removeInternByIdTask =
+                this.internService.RemoveInternByIdAsync(inputInternId);
+
+            InternValidationException actualInternValidationException =
+                await Assert.ThrowsAsync<InternValidationException>(
+                    removeInternByIdTask.AsTask);
+
+            // then
+            actualInternValidationException.Should()
+                .BeEquivalentTo(expectedInternValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectInternByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionsAs(
+                    expectedInternValidationException))),
+                        Times.Once());
+                    
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteInternAsync(It.IsAny<Intern>()),
+                    Times.Never());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
