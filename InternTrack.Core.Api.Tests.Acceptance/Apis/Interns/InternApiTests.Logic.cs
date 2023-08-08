2b680d3cc03701c2ef8b82cff41fb2317fd043f0
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-07 22:09:49 -0700

    ShouldPostInternAsync -> PASS

--- InternTrack.Core.Api.Tests.Acceptance/Apis/Interns/InternApiTests.Logic.cs
+++ InternTrack.Core.Api.Tests.Acceptance/Apis/Interns/InternApiTests.Logic.cs
@@ -11,6 +11,27 @@ namespace InternTrack.Core.Api.Tests.Acceptance.Apis.Interns
 {
     public partial class InternApiTests
     {
+        [Fact]
+        public async Task ShouldPostInternAsync()
+        {
+            //given
+            Intern randomIntern = CreateRandomIntern();
+            Intern inputIntern = randomIntern;
+            Intern expectedIntern = randomIntern;
 
+            //when
+            await this.internTrackApiBroker
+                .PostInternAsync(inputIntern);
+
+            Intern actualIntern =
+                await this.internTrackApiBroker
+                    .GetInternByIdAsync(inputIntern.Id);
+
+            //
+            actualIntern.Should().BeEquivalentTo(expectedIntern);
+
+            await this.internTrackApiBroker
+                .DeleteInternByIdAsync(inputIntern.Id);
+        }
     }
 }
