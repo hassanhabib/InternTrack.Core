0b5758be3ebc29555f0526b7700f46b8821a1c6a
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-07 22:00:22 -0700

    CODERUB: Removed duplicated packages and upadated System.Text.Json

--- InternTrack.Core.Api.Tests.Acceptance/Brokers/InternTrackApiBroker.Interns.cs
+++ InternTrack.Core.Api.Tests.Acceptance/Brokers/InternTrackApiBroker.Interns.cs
@@ -4,6 +4,7 @@
 // -------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using InternTrack.Core.Api.Models.Interns;
 
@@ -18,6 +19,12 @@ namespace InternTrack.Core.Api.Tests.Acceptance.Brokers
 
         public async ValueTask<Intern> GetInternByIdAsync(Guid internId) =>
             await this.apiFactoryClient.GetContentAsync<Intern>($"{InternRelativeString}/{internId}");
+
+        public async ValueTask<List<Intern>> GetAllInternsAsync() =>
+            await this.apiFactoryClient.GetContentAsync<List<Intern>>($"{InternRelativeString}/");
+
+        public async ValueTask<Intern> PutInternAsync(Intern intern) =>
+            await this.apiFactoryClient.PutContentAsync<Intern>(InternRelativeString, intern);
 
         public async ValueTask<Intern> DeleteInternByIdAsync(Guid internId) =>
             await this.apiFactoryClient.DeleteContentAsync<Intern>($"{InternRelativeString}/{internId}");
