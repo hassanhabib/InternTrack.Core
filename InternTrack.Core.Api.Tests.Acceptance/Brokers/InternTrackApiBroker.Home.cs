c45dfb711d19bf51c3452f069982dc9092ad5c8b
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-08 10:26:43 -0700

    ACCEPTANCE: Home Broker Intialized

---
+++ InternTrack.Core.Api.Tests.Acceptance/Brokers/InternTrackApiBroker.Home.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternTrack.Core.Api.Tests.Acceptance.Brokers
+{
+    public partial class InternTrackApiBroker
+    {
+        private const string HomeRelativeUrl = "api/home";
+
+        public async ValueTask<string> GetHomeMessage() =>
+            await this.apiFactoryClient.GetContentStringAsync(HomeRelativeUrl);
+    }
+}
