e8194df2319a1abc80de7676169a11a8dff3f77e
Pwongchaiya <Pwongchaiya1@gmail.com>
2023-08-08 21:40:04 -0700

    CODERUB: Capitalized Intern in LockedInternException

--- InternTrack.Core.Api/Models/Interns/Exceptions/LockedInternException.cs
+++ InternTrack.Core.Api/Models/Interns/Exceptions/LockedInternException.cs
@@ -11,7 +11,7 @@ namespace InternTrack.Core.Api.Models.Interns.Exceptions
     public class LockedInternException : Xeption
     {
         public LockedInternException(Exception innerException)
-            : base(message: "Locked intern record exception, please try again later.", innerException)
+            : base(message: "Locked Intern record exception, please try again later.", innerException)
         { }
 
         public LockedInternException(string message, Exception innerException)
